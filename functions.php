49967500709193380322caca5b8e506b20aa8e9a
Ariel Kuhn <abpoker01@gmail.com>
2014-11-09 06:00:57 -0300

    200

--- functions.php
+++ functions.php
@@ -94,15 +94,31 @@ add_action( 'widgets_init', 'garua_widgets_init' );
 function garua_scripts() {
 	wp_enqueue_style( 'garua-style', get_stylesheet_uri() );
 
+	wp_deregister_script( 'jquery' );
+	$jquery_cdn = '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';
+	wp_enqueue_script( 'jquery', $jquery_cdn, array(), '20100226', true );
+
 	wp_enqueue_script( 'garua-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'garua-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	
+	wp_enqueue_script( 'garua-match-height', get_template_directory_uri() . '/js/matchHeight.js', array(), '20140001', true );
+	
+	wp_enqueue_script( 'garua-scripts', get_template_directory_uri() . '/js/scripts.js', array(), '20140001', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'garua_scripts' );
+/**
+ * Read More tag Customize
+ */
+
+add_filter( 'the_content_more_link', 'modify_read_more_link' );
+function modify_read_more_link() {
+return '<a class="more-link" href="' . get_permalink() . '">'.__( 'Read More', 'garua' ).'</a>';
+}
 
 /**
  * Implement the Custom Header feature.
@@ -128,3 +144,6 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+
--- header.php
+++ header.php
@@ -28,11 +28,12 @@
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</div><!-- .site-branding -->
+		
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Primary Menu', 'garua' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- #site-navigation -->
 		</div>
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'garua' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
