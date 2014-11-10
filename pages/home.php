0ee7019254b5e4f48554ecee3eaeaf09e7fd22c8
Ariel Kuhn <abpoker01@gmail.com>
2014-11-10 01:26:11 -0300

    210

---
+++ pages/home.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+
+<?php get_header(); ?>
+</div> <!-- End main page -->
+<div class="frontpage">
+	<div class="slider">
+		<ul class="slider-container">
+			<li class="image1">
+				ble
+			</li>
+		</ul>
+	</div>
+
+
+	<ul class="headings clearfix">
+		<li class="heading1"> 
+			<img></img>
+			<h2>Title</h2>
+			<p>Description</p>
+		</li>
+		<li class="heading2"> 
+			<img></img>
+			<h2>Title</h2>
+			<p>Description</p>
+		</li>
+		<li class="heading3"> 
+			<img></img>
+			<h2>Title</h2>
+			<p>Description</p>
+		</li>
+	</ul>
+	<ul class="blogAdvance">
+		<li class="blogAdvanceChild1">
+			blog note
+		</li>
+	</ul>
+</div>
+
+
+
+<?php get_footer(); ?>
